3086a5b75bc4cb76d0bb72c99575f0d2cd81cd5e
rlie2021 <reagan@krystl.net>
2022-05-26 20:09:44 +0800

    Week1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,20 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='djangoapp/templates/about/', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path('get_dealer_details/', views.get_dealer_details, name='get_dealer_details'),
     # path for add a review view
-
+    path('add_review/', views.add_review, name='add_review'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
